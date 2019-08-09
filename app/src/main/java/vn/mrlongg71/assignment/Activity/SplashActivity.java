9dd4286bb905e4a0ab7716186bcbb609cfc2af88
MrLongg71 <41661101+MrLongg71@users.noreply.github.com>
2019-08-09 22:14:19 +0700

    Splash screens viewpager

--- app/src/main/java/vn/mrlongg71/assignment/Activity/SplashActivity.java
+++ app/src/main/java/vn/mrlongg71/assignment/Activity/SplashActivity.java
@@ -1,27 +1,208 @@
 package vn.mrlongg71.assignment.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import vn.mrlongg71.assignment.R;
+import vn.mrlongg71.assignment.View.PrefManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    PrefManager prefManager;
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    MyViewPagerAdapter myViewPagerAdapter;
+    Button btnSkip, btnNext;
+    int layouts[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefManager = new PrefManager(SplashActivity.this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    anhxa();
+                    dotsLayout.setBackgroundResource(R.drawable.start);
+                    btnNext.setVisibility(View.GONE);
+                    btnSkip.setVisibility(View.GONE);
+                    launchHomeScreen();
+
+                    finish();
+
+                }
+            }, 1000);
+
+        }
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+        anhxa();
+
+
+    }
+
+    private void anhxa() {
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        layouts = new int[]{
+                R.layout.start1,
+                R.layout.start2,
+                R.layout.start3};
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(i);
-                // close this activity
-                finish();
+            public void onClick(View v) {
+                launchHomeScreen();
             }
-        }, 3000);
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+
+    }
+
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+
+            if (position == layouts.length - 1) {
+                btnNext.setText("Start");
+                btnSkip.setVisibility(View.GONE);
+            } else {
+                btnNext.setText("Next");
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+    /**
+     * View pager Anim
+     */
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0f);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0f);
+            }
+        }
     }
 }
+
